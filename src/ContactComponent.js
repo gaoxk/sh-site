c2f920855698b80d4b8de2bfa5b7c1e12a037f47
gaoxk <gaoxuekristy@gmail.com>
2018-07-11 17:19:42 -0400

    Basic contact done (no validation, ugly buttons)

--- src/ContactComponent.js
+++ src/ContactComponent.js
@@ -8,34 +8,74 @@ class ContactComponent extends Component {
       this.state = {
         name: '',
         email: '',
-        message: ''
-      }
+        message: '',
+        news: ''
+      };
+      this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(e, type) {
+      type === 'name' && this.setState({ name: e.target.value });
+      type === 'email' && this.setState({ email: e.target.value });
+      type === 'message' && this.setState({ message: e.target.value });
+      type === 'news' && this.setState({ news: e.target.value });
     }
     render() {
         return (
             <div className='container contact'>
               <h1>{ResourceStrings.contact}</h1>
-              <form>
-                <FormGroup
-                  controlId="formBasicText"
-                >
-                  <ControlLabel>{ResourceStrings.contact_name}</ControlLabel>
-                  <FormControl
-                    type="text"
-                    value={this.state.name}
-                  />
-                  <ControlLabel>{ResourceStrings.contact_email}</ControlLabel>
-                  <FormControl
-                    type="text"
-                    value={this.state.email}
-                  />
-                  <ControlLabel>{ResourceStrings.contact_message}</ControlLabel>
-                  <FormControl
-                    componentClass="textarea"
-                    value={this.state.message}
-                  />
-                </FormGroup>
-              </form>
+              <div className='row' >
+                <div className='col-md-6'>
+                  <form>
+                    <FormGroup
+                      controlId='formBasicText'
+                    >
+                      <ControlLabel><p>{ResourceStrings.contact_name}</p></ControlLabel>
+                      <FormControl
+                        type='text'
+                        value={this.state.name}
+                        onChange={(e) => this.handleChange(e, 'name')}
+                      />
+                      <ControlLabel><p>{ResourceStrings.contact_email}</p></ControlLabel>
+                      <FormControl
+                        type='text'
+                        value={this.state.email}
+                        onChange={(e) => this.handleChange(e, 'email')}
+                      />
+                      <ControlLabel><p>{ResourceStrings.contact_message}</p></ControlLabel>
+                      <FormControl
+                        componentClass='textarea'
+                        value={this.state.message}
+                        onChange={(e) => this.handleChange(e, 'message  ')}
+                      />
+                      <a href='#contact'>
+          		          <button type='button' className='btn'>
+                            <h4>{ResourceStrings.send.toUpperCase()}</h4>
+                        </button>
+                      </a>
+                    </FormGroup>
+                  </form>
+                </div>
+                <div className='col-md-6'>
+                  <div className='newsletter'>
+                    <h2>{ResourceStrings.news}</h2>
+                    <p>{ResourceStrings.news_desc}</p>
+                    <form>
+                      <FormGroup controlId='formBasicText'>
+                        <FormControl
+                          type='text'
+                          value={this.state.news}
+                          onChange={(e) => this.handleChange(e, 'news')}
+                        />
+                        <a href='#contact'>
+            		          <button type='button' className='btn'>
+                              <h4>{ResourceStrings.news_sub.toUpperCase()}</h4>
+                          </button>
+                        </a>
+                      </FormGroup>
+                    </form>
+                  </div>
+                </div>
+              </div>
           </div>
         );
     }
