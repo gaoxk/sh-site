00015a18dde39ea4f2544c4bb5aaad2c70315967
gaoxk <gaoxuekristy@gmail.com>
2018-07-30 21:43:43 -0400

    'how it works' nav complete

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import ResourceStrings from './../ResourceStrings';
 
 import plan from './../assets/Plan.png';
-import proto from './../assets/ideal.png';
-import present from './../assets/logo.png';
+import proto from './../assets/Prototype.png';
+import present from './../assets/ideal.png';
 
 class HowItWorks extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			visible: [
-				'',
+				'here',
 				'nope',
 				'nope'
+			],
+			colors: [
+				'#3c56a5',
+				'#ec1b69',
+				'#80c241'
 			]
 		};
 		this.handleClick = this.handleClick.bind(this);
@@ -21,7 +26,7 @@ class HowItWorks extends Component {
 	handleClick(num) {
 		console.log(num);
 		let i;
-		let arr = ['' , '', ''];
+		let arr = ['here' , 'here', 'here'];
 		for(i=0; i<3; i++) {
 			if(i !== num) arr[i]='nope';
 		}
@@ -34,7 +39,7 @@ class HowItWorks extends Component {
 		let i;
 		for(i=0; i<3; i++){
 			imgs.push(
-				<img src={assets[i]} className={this.state.visible[i]}/>
+				<img src={assets[i]} className={this.state.visible[i] + ' imger'}/>
 			);
 		}
 		return (
@@ -44,14 +49,23 @@ class HowItWorks extends Component {
 						<h1>{ResourceStrings.how_it_works}</h1>
 						<p>{ResourceStrings.how_in_24}</p>
 						<div className='row'>
-							<div className='col-md-2' onClick={() => this.handleClick(0)}>
-								<h3>{ResourceStrings.how_plan}</h3>
+							<div
+								className={'col-md-4 ' + this.state.visible[0] + '-nav blue'}
+								style={{color: this.state.colors[0]}}
+								onClick={() => this.handleClick(0)}>
+								<h4>{ResourceStrings.how_plan}</h4>
 							</div>
-							<div className='col-md-2' onClick={() => this.handleClick(1)}>
-      					<h3>{ResourceStrings.how_proto}</h3>
+							<div
+								className={'col-md-4 ' + this.state.visible[1] + '-nav red'}
+								style={{color: this.state.colors[1]}}
+								onClick={() => this.handleClick(1)}>
+      					<h4>{ResourceStrings.how_proto}</h4>
       				</div>
-							<div className='col-md-2' onClick={() => this.handleClick(2)}>
-      					<h3>{ResourceStrings.how_present}</h3>
+							<div
+								className={'col-md-4 ' + this.state.visible[2] + '-nav green'}
+								style={{color: this.state.colors[2]}}
+								onClick={() => this.handleClick(2)}>
+      					<h4>{ResourceStrings.how_present}</h4>
       				</div>
 						</div>
 					</div>
