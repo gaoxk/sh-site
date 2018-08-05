a8899ed1b3da46d35ab5e10a1498f16f0951dc25
gaoxk <gaoxuekristy@gmail.com>
2018-08-05 19:28:02 -0400

    phew my computer gave me a blue screen of death so this is a just-in-case commit

--- src/server/index.js
+++ src/server/index.js
@@ -10,32 +10,42 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('dist'));
 
 app.post('/contact', (req, res) => {
+	console.log('can anyone hear me?');
+
 	console.log(req.body);
 
-	let transporter = nodemailer.createTransport({
-  	service: 'mail.starterhacks.ca',
+	const transportConfig = {
+		host: 'imap.starterhacks.ca',
   	auth: {
   		user: 'contact@starterhacks.ca',
-  		pass: 'some password'
+  		pass: '@Milesteller2018'
   	}
-	});
+	};
 
-	let sendEmail = {
-  	from: req.email,
+	const sendEmail = {
+  	from: 'contact@starterhacks.ca',
   	to: 'contact@starterhacks.ca',
   	subject: req.subject,
   	text: req.message
 	};
-  
-	/* to be completed
-  transporter.sendMail(sendEmail, (error, info) => {
+
+	const transporter = nodemailer.createTransport(transportConfig);
+
+	transporter.verify( (error, success) => {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log('Server is ready to take our messages');
+		}
+	});
+
+	transporter.sendMail(sendEmail, (error, info) => {
   	if (error) {
   		console.log(error);
   	} else {
   		console.log('Email sent: ' + info.response);
   	}
-  });
-  */
+	});
 });
 
 app.listen(8080, () => console.log('Listening on port 8080!'));
