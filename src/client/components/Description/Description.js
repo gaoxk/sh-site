63502701a7bf7cea2562edc29aed545aa9f22715
gaoxk <gaoxuekristy@gmail.com>
2018-08-11 23:23:28 -0400

    tablet compatible

--- src/client/components/Description/Description.js
+++ src/client/components/Description/Description.js
@@ -5,10 +5,10 @@ import { Element } from 'react-scroll';
 const Description = props => (
 	<Element name={'description-' + props.id}>
 		<div className='description row container-fluid' id={props.id}>
-			<div className='col-md-6'>
+			<div className='col-sm-6'>
 				<img id={props.id} src={props.imger} alt='about StarterHacks'/>
 			</div>
-			<div className='col-md-6'>
+			<div className='col-sm-6'>
 				<div className='container'>
 					<div className='description-text'>
 						<h1>{props.title}</h1>
